db25c81522d8be9dd03aa39228e8311f4c66f7d3
kolya <kolyad3v@pm.me>
2023-07-19 19:56:22 +0100

    edits

--- blockchainCreator/Block.cs
+++ blockchainCreator/Block.cs
@@ -1,3 +1,6 @@
+using System.Security.Cryptography;
+using System.Text;
+
 namespace blockchainCreator;
 
 public class Block
@@ -8,11 +11,24 @@ public class Block
     public string PreviousHash { get; set;}
     public string Hash {get; set;}
 
-    public Block(DateTime Timestamp, string PreviousHash, string data){
+    public Block(DateTime timestamp, string previousHash, string data){
         Index = 0;
         Timestamp = timestamp;
-        
-    }  
+        PreviousHash = previousHash;
+        Data = data;
+        Hash = CalculateHash();
+    }
+
+    public string CalculateHash()
+    {
+        SHA256 sha256 = SHA256.Create();
+
+        byte[] inputBytes = Encoding.ASCII.GetBytes($"{Timestamp}-{PreviousHash ?? ""}-{Data}");
+        byte[] outputBytes = sha256.ComputeHash(inputBytes);
+
+        return Convert.ToBase64String(outputBytes);
+
+    }
     
 
     
